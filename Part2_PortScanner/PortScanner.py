813b73b30410805befe03de686bc39978c0e7516
Gregory.Guzman <g.guzman6135@student.nu.edu>
2026-06-18 23:10:05 -0700

    Added error handling, and defined PORT LIST and PORT RANGE mode

--- Part2_PortScanner/PortScanner.py
+++ Part2_PortScanner/PortScanner.py
@@ -4,12 +4,34 @@
 import socket
 
 # identify target and port range
-TARGET = "127.0.0.1" # localhost for testing purposes. In real scenarios, this would be the IP address of the target machine.
-START_PORT = 5550
-END_PORT = 5560
-print(f"Scanning {TARGET} for open ports in range {START_PORT}-{END_PORT}...")
+TARGET = "scanme.nmap.org" # localhost for testing purposes. In real scenarios, this would be the IP address of the target machine. ONLY SCAN LOCAL HOST OR scanme.nmap.org OR IP ADDRESSES THAT YOU OWN.
+START_PORT = 80
+END_PORT = 100
+PORT_LIST = [21, 22, 80, 443] # This is a different variable than the range. Its a list of specific ports. To scan these, change the for loop to "for port in PORT_LIST:"
 
-for port in range(START_PORT, END_PORT +1): # need to add 1 to include the END_PORT in the range because Python excludes the end value.
+# Before we start, we should make sure to define the rules. If anything is outside of the rules, print a message and exit.
+# validate input.
+if START_PORT < 1:
+    print("Invalid start port. Port numbers must be between 1 and 65535.")
+    exit()
+if END_PORT > 65535:
+    print("Invalid end port. Port numbers must be between 1 and 65535.")
+    exit()
+if END_PORT < START_PORT:
+    print("Invalid port range. End port must be greater than or equal to start port.")
+    exit()
+# Validate that the target is a valid address. Target can be IP or Hostname.
+try:
+    socket.gethostbyname(TARGET)
+except socket.gaierror:
+    print(f"{TARGET} is an invalid target. Please enter a valid IP address or hostname.")
+    exit()
+
+#print(f"Valid target and port range. Scanning {TARGET} for open ports in range {START_PORT}-{END_PORT}...") #PORT RANGE MODE
+print(f"Valid target and port range. Scanning {TARGET} for open ports in range {PORT_LIST}...") 
+
+for port in PORT_LIST: #PORT LIST MODE
+#for port in range(START_PORT, END_PORT +1): # PORT RANGE MODE ... Need to +1 to include the END_PORT in the range because Python excludes the end value.
     # Before you can make a call, you need a phone. Create a socket.
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # You make a call and the phone rings and rings, but never gets answered or hangs up. Before you get started, its best to know how long you will wait before you give up (in seconds)?
