c2405e4f5a88c5054acdb5b204cba246de447647
Gregory.Guzman <g.guzman6135@student.nu.edu>
2026-06-18 23:12:30 -0700

    Minor change to defaults.

--- Part2_PortScanner/PortScanner.py
+++ Part2_PortScanner/PortScanner.py
@@ -4,7 +4,7 @@
 import socket
 
 # identify target and port range
-TARGET = "scanme.nmap.org" # localhost for testing purposes. In real scenarios, this would be the IP address of the target machine. ONLY SCAN LOCAL HOST OR scanme.nmap.org OR IP ADDRESSES THAT YOU OWN.
+TARGET = "127.0.0.1" # localhost for testing purposes. In real scenarios, this would be the IP address of the target machine. ONLY SCAN LOCAL HOST OR scanme.nmap.org OR IP ADDRESSES THAT YOU OWN.
 START_PORT = 80
 END_PORT = 100
 PORT_LIST = [21, 22, 80, 443] # This is a different variable than the range. Its a list of specific ports. To scan these, change the for loop to "for port in PORT_LIST:"
@@ -31,7 +31,9 @@ except socket.gaierror:
 print(f"Valid target and port range. Scanning {TARGET} for open ports in range {PORT_LIST}...") 
 
 for port in PORT_LIST: #PORT LIST MODE
+
 #for port in range(START_PORT, END_PORT +1): # PORT RANGE MODE ... Need to +1 to include the END_PORT in the range because Python excludes the end value.
+ 
     # Before you can make a call, you need a phone. Create a socket.
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # You make a call and the phone rings and rings, but never gets answered or hangs up. Before you get started, its best to know how long you will wait before you give up (in seconds)?
